7fb338fdc096565080c0550675052cd884568bda
Alexey Karapetov <karapetov@gmail.com>
2017-03-03 18:48:30 -0800

    Map prototype

--- public/s/map.js
+++ public/s/map.js
@@ -1,14 +1,36 @@
-function initMap() {
-    var myLatLng = {lat: -25.363, lng: 131.044};
+(function () {
+    "use strict";
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: myLatLng
-    });
+    window['initMap'] = function() {
+        let myLatLng = {lat: -25.363, lng: 131.044};
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-    });
-}
+        let map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        const mutate = function(p) {
+            const d_lat = 0.03;
+            const d_lng = -0.05;
+            return {
+                lat: p.lat + Math.random() * d_lat,
+                lng: p.lng + Math.random() * d_lng,
+            };
+        };
+        let markers = [];
+        setInterval(
+            function() {
+                markers.map(m => m.setMap(null));
+                markers = [];
+                for (let i = 0; i < 1000; i++) {
+                    markers.push(new google.maps.Marker({
+                        position: mutate(myLatLng)
+                    }))
+                }
+                markers.map(m => m.setMap(map));
+            },
+            1000
+        );
+    };
+})();
+
