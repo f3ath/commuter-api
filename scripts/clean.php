e4b5345887b5417fd8e74e70936f7cb5d480ffa0
Alexey <f3ath@users.noreply.github.com>
2017-03-11 00:28:47 -0800

    Update clean.php

--- scripts/clean.php
+++ scripts/clean.php
@@ -1,4 +1,4 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__. '/../vendor/autoload.php';
-$storage = \F3\Commuter\DI::fromEnv()['storage']->clean();
+\F3\Commuter\DI::fromEnv()['storage']->clean();
